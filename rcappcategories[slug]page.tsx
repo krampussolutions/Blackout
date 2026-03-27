93f5be54fd923637d7c2a28fd36e7dbfc8b951e0
Matthew Poteet <krampussolutions@gmail.com>
2026-03-26 21:40:00 -0400

    Remove accidental root TSX file

--- rcappcategories[slug]page.tsx
+++
@@ -1,5 +0,0 @@
-[33m0031f09[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m patch cat.
-[33mdc4bafc[m Fix OG image content type for Facebook preview
-[33m07b01d7[m og image
-[33m0f49fdc[m og image
-[33m9ba9072[m og image
